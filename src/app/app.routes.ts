469ef52793590522d7f87890113c450cb2575a60
Vasiliy <vasilisrusgame@gmail.com>
2026-08-04 17:34:48 +0500

    fix: add title for open-graph route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,4 @@
 import { Routes } from '@angular/router';
 import { Inline } from './inline/inline';
 
-export const routes: Routes = [{ path: 'open-graph', component: Inline }];
+export const routes: Routes = [{ path: 'open-graph', component: Inline, title: 'Open Graph Page' }];
