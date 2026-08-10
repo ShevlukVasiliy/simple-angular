4ce310e278a0ef910e73fc22346e4c2eda633b59
Vasiliy <vasilisrusgame@gmail.com>
2026-08-10 14:26:54 +0500

    feat: show data from response-service

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ResponseService } from './response-service';
+import { Response } from './interface/response';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  providers: [ResponseService],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {}
+export class App {
+  constructor(public response: ResponseService) {}
+}
