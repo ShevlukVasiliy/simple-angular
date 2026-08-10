bba566f6228f7ce6469ee8a6f85ec369d5af0e00
Vasiliy <vasilisrusgame@gmail.com>
2026-08-10 14:32:44 +0500

    fix: remove unused code

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 import { ResponseService } from './response-service';
-import { Response } from './interface/response';
 
 @Component({
   selector: 'app-root',
-  providers: [ResponseService],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
