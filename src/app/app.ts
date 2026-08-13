95de1b951c4a2be098af12c087f67d0d7cdbc675
Vasiliy <vasilisrusgame@gmail.com>
2026-08-13 19:41:54 +0500

    feat: add one observable and see flags (add or not values based on these flags)

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, signal } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {}
+export class App {
+  public randomNumbers = signal<string[]>([]);
+  public orderedNumbers = signal<number[]>([]);
+
+  public isRandomRun = signal<boolean>(true);
+  public isOrderedRun = signal<boolean>(true);
+
+  public sub!: Subscription;
+
+  constructor() {
+    this.subscribe();
+  }
+
+  subscribe() {
+    this.sub = interval(2000).subscribe(() => {
+      if (this.isRandomRun()) {
+        this.addToRandomNumbers(this.genRandomNumber());
+      }
+      if (this.isOrderedRun()) {
+        var newValue = this.orderedNumbers().length + 1;
+        this.addToOrderedNumbers(newValue);
+      }
+    });
+  }
+
+  addRandom() {
+    this.isRandomRun.update(() => true);
+  }
+  addOrdered() {
+    this.isOrderedRun.update(() => true);
+  }
+  dontAddRandom() {
+    this.isRandomRun.update(() => false);
+  }
+  dontAddOrdered() {
+    this.isOrderedRun.update(() => false);
+  }
+
+  public addToRandomNumbers(value: number) {
+    this.randomNumbers.update((old) => [...old, `Random Value: ${value}`]);
+  }
+  public addToOrderedNumbers(value: number) {
+    this.orderedNumbers.update((old) => [...old, value]);
+  }
+
+  public genRandomNumber(): number {
+    return Math.floor(Math.random() * 1000);
+  }
+}
