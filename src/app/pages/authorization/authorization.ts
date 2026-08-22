416b06e3010435671c79019eb92f06c8868ac797
Vasiliy <vasilisrusgame@gmail.com>
2026-08-22 15:00:32 +0500

    feat: make layout for registration and authorization pages

--- src/app/pages/authorization/authorization.ts
+++ src/app/pages/authorization/authorization.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 import { BaseLayout } from '../../layout/base-layout/base-layout';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { NgClass } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-authorization',
-  imports: [BaseLayout],
+  imports: [BaseLayout, NgClass, ReactiveFormsModule, RouterLink],
   templateUrl: './authorization.html',
   styleUrl: './authorization.css',
 })
-export class Authorization {}
+export class Authorization {
+  public form: FormGroup = new FormGroup({
+    login: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    fastSession: new FormControl(false),
+  });
+
+  submit() {
+    console.log(this.form.getRawValue());
+  }
+}
--- src/app/pages/registration/registration.ts
+++ src/app/pages/registration/registration.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 import { BaseLayout } from '../../layout/base-layout/base-layout';
+import { NgClass } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
-  imports: [BaseLayout],
+  imports: [BaseLayout, NgClass, ReactiveFormsModule],
   templateUrl: './registration.html',
   styleUrl: './registration.css',
 })
-export class Registration {}
+export class Registration {
+  public form: FormGroup = new FormGroup({
+    login: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required]),
+    middleName: new FormControl('', []),
+  });
+
+  submit() {
+    console.log(this.form.getRawValue());
+  }
+}
