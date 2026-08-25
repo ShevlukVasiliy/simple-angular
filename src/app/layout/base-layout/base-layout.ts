fa86f419911e202735c8d1b7e5ab229eaa67cb6d
Vasiliy <vasilisrusgame@gmail.com>
2026-08-25 06:24:16 +0500

    feat: persistent bottom notification banner

--- src/app/layout/base-layout/base-layout.ts
+++ src/app/layout/base-layout/base-layout.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Footer } from '../footer/footer';
 import { Header } from '../header/header';
+import { BottomNotification } from '../../uikit/components/bottom-notification/bottom-notification';
 
 @Component({
   selector: 'app-base-layout',
-  imports: [Footer, Header],
+  imports: [Footer, Header, BottomNotification],
   templateUrl: './base-layout.html',
   styleUrl: './base-layout.css',
 })
--- src/app/uikit/components/bottom-notification/bottom-notification.ts
+++ src/app/uikit/components/bottom-notification/bottom-notification.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Store } from '@ngxs/store';
+import { CloseBottomNotification } from '../../../store/notification.model';
+import { NotificationState } from '../../../store/notification.state';
 
 @Component({
   selector: 'app-bottom-notification',
@@ -6,4 +9,12 @@ import { Component } from '@angular/core';
   templateUrl: './bottom-notification.html',
   styleUrl: './bottom-notification.css',
 })
-export class BottomNotification {}
+export class BottomNotification {
+  private store = inject(Store);
+
+  public isClosed = this.store.selectSignal(NotificationState.isBottomClosed);
+
+  close(): void {
+    this.store.dispatch(new CloseBottomNotification());
+  }
+}
